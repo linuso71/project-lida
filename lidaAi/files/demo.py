663065f4eb8db286f27e0721ff0d0382d799b1be
linus <84990808+linuso71@users.noreply.github.com>
2024-02-05 02:58:32 +0530

    Update demo.py

--- lidaAi/files/demo.py
+++ lidaAi/files/demo.py
@@ -5,7 +5,7 @@ from lida import Manager, TextGenerationConfig, llm
 from lida.datamodel import Goal
 from lida.utils import plot_raster
 
-lida1 = Manager(text_gen = llm("openai",api_key = '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'))
+lida1 = Manager(text_gen = llm("openai",api_key = openapikey))
 
 
 def save_uploaded_csv_file(file):
@@ -24,19 +24,19 @@ def save_uploaded_csv_file(file):
 
 def get_csv_summary(filename):
     
-    lida1 = Manager(text_gen = llm("openai",api_key = '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'))
+    lida1 = Manager(text_gen = llm("openai",api_key = openapikey))
     textgen_config = TextGenerationConfig(n=1,temperature=0.2,model='gpt-3.5-turbo',use_cache=True)
     summary = lida1.summarize(filename,textgen_config=textgen_config)
     return summary
 
 def get_goals(filename):
-    lida1 = Manager(text_gen = llm("openai",api_key = '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'))
+    lida1 = Manager(text_gen = llm("openai",api_key = openapikey))
     summary = get_csv_summary(filename)
     goals = lida1.goals(summary, n=5)
     return goals
 
 def get_visualization(filename,query):
-    lida1 = Manager(text_gen = llm("openai",api_key = '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'))
+    lida1 = Manager(text_gen = llm("openai",api_key = openapikey))
     textgen_config = TextGenerationConfig(n=1,temperature=0.7,model='gpt-3.5-turbo',use_cache=True)
 
     summary = lida1.summarize(filename,textgen_config=textgen_config)
